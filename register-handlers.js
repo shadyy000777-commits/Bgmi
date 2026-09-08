1d6fabdf5456251e98f30dfa445285389403a885
shadyy000777-commits <shadyy000777@gmail.com>
2026-09-08 09:43:52 +0530

    Update register-handlers.js

--- register-handlers.js
+++ register-handlers.js
@@ -433,8 +433,8 @@ async function handleManageMatchesButton(interaction) {
 
   const row = new ActionRowBuilder().addComponents(
     new ButtonBuilder()
-      .setCustomId('register_Manage_slot')
-      .setLabel('change slot')
+      .setCustomId('register_cancel_slot')
+      .setLabel('Manage slot')
       .setEmoji('🔄')
       .setStyle(ButtonStyle.Secondary),
     new ButtonBuilder()
